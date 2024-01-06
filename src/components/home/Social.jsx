2cd57144fb13f225ba0293709dbe366be5afb202
Abed Bashir <56051101+AbedBashir@users.noreply.github.com>
2024-01-06 13:22:38 +0200

    Home Section

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/" className="home__social-icon" target='_blank'>
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/abdel-rahman-bashir-952732208/" className="home__social-icon" target='_blank'>
+        <i className="uil uil-linkedin"></i>
+      </a>
+      <a href="https://github.com/AbedBashir" className="home__social-icon" target='_blank'>
+        <i className="uil uil-github"></i>
+      </a>
+    </div>
+  )
+}
+
+export default Social
