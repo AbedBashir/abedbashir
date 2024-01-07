ece1e372b0657add64c75f972300a8a427dfe708
Abed Bashir <56051101+AbedBashir@users.noreply.github.com>
2024-01-07 13:08:07 +0200

    Testimonials

---
+++ src/components/testimonials/Data.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export const Data =  [
+  {
+    id: 1,
+    title: "Jad Kawas",
+    description:
+      "Abed and I worked together on my law online library, so i can have all my archive cases in 1 place. the database is well structured, the UI of the app is very user-friendly, and the communication with Abed was amazing throughout all the process",
+  },
+  {
+    id: 2,
+    title: "Ibrahim Hamdan",
+    description:
+      "As a coach, I needed some templates and consultations for online scheduling and training planning. I got introduced to Abed, and step by step we achieved what I needed. The project was on point and I believe all the trainings and explanations given by Abed is worth every penny.",
+  },
+  {
+    id: 3,
+    title: "Moussa Arbid",
+    description:
+      "Abed is a very passionate person. I was his technical lead and he always showed me consistency in his work, attention to details, and most important thing, his honesty in his work. I couldn't be more proud of him for years. Cheers!",
+  },
+];
